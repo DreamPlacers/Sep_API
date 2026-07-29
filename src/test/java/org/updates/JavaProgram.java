5d2f08b6b3c3df882eedefe4c74c99a9dfbb1c50
pavinjaivarshan6 <pavinjaivarshan6@gmail.com>
2026-07-29 11:46:11 +0530

    Updated javaProgram

--- src/test/java/org/updates/JavaProgram.java
+++ src/test/java/org/updates/JavaProgram.java
@@ -15,6 +15,34 @@ public class JavaProgram {
             }
 
         }
+        
+        
+        
+        
+        
+       for (int n=0; n<1000; n++) {
+    	   int i=0,j=0,a=0;
+    	   a=n;
+    	   while(a>0) {
+    		  i=a%10;
+    		  j=j+(i*i*i);
+    		  a=a/10;
+    	   }
+    	   if(n==j) {
+    		   System.out.println(j);
+    		   System.out.println("its is a Armstrong Number");
+    	   }
+       }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 }
