ce06dda0817d864bee8c40dbdd3cabaebfabcde4
Wenses07 <vikashrajk20@gmail.com>
2026-09-14 13:25:32 +0530

    Updated

---
+++ src/test/java/com/Vikash/Java.java
@@ -0,0 +1,18 @@
+package com.Vikash;
+
+public class Java {
+	
+	public void add() {
+		int a = 10;
+		int b = 20;
+		
+		int c = a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		Java obj = new Java();
+		obj.add();
+		
+	}
+}
